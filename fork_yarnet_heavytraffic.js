3268a1c1ef17d0007952805a8a0618bb8c81823e
a <a@a.com>
2026-02-02 22:51:46 +0800

    add new domains to 'movie' group

--- fork_yarnet_heavytraffic.js
+++ fork_yarnet_heavytraffic.js
@@ -431,6 +431,7 @@ const serviceConfigs = [
     rules: [
       "DOMAIN-SUFFIX,missav.ai,电影",
       "DOMAIN-SUFFIX,surrit.com,电影",
+      "DOMAIN-SUFFIX,nineyu.com,电影",
       "DOMAIN-SUFFIX,javdb.com,电影",
       "DOMAIN-SUFFIX,jdbstatic.com,电影",
     ],
